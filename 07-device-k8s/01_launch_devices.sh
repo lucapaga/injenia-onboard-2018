fc32e33312c7cab737fe8511f64809ca47366d6a
Luca Paganelli <luca.paganelli@injenia.it>
2018-03-05 15:25:10 +0100

    preparing to launch

--- 07-device-k8s/01_launch_devices.sh
+++ 07-device-k8s/01_launch_devices.sh
@@ -9,17 +9,83 @@
 #  - sudo mv ./kubectl /usr/local/bin/kubectl
 #
 
-#gcloud container clusters get-credentials --zone ${k8s_cluster_eu_zone} ${k8s_cluster_eu_name}
-
-mkdir -p europe
-for aCity in $(cat ../06-device/data/europa.txt); do
-  export CUSTER_REGION=europe
+echo "========================================================================="
+echo ""
+echo "  Launching EUROPEAN devices ..."
+echo ""
+echo "-------------------------------------------------------------------------"
+DEVICES_ZONE=europe
+TXT_FILE_NAME=europa
+gcloud container clusters get-credentials --zone ${k8s_cluster_eu_zone} ${k8s_cluster_eu_name}
+mkdir -p ${DEVICES_ZONE}
+rm -rf ${DEVICES_ZONE}/*
+for aCity in $(cat ../06-device/data/${TXT_FILE_NAME}.txt); do
+  export CUSTER_REGION=${DEVICES_ZONE}
   export CITY=${aCity}
   export CITY_LOWER=$(echo ${aCity} | tr '[:upper:]' '[:lower:]')
-  cat device-tpl.yaml | envsubst > europe/k8s-job-${CITY_LOWER}.yaml
+  cat device-tpl.yaml | envsubst > ${DEVICES_ZONE}/k8s-job-${CITY_LOWER}.yaml
 done
+kubectl apply -f ${DEVICES_ZONE}
+echo "-------------------------------------------------------------------------"
 
-# kubectl apply -f europe/*
+
+echo "========================================================================="
+echo ""
+echo "  Launching US devices ..."
+echo ""
+echo "-------------------------------------------------------------------------"
+DEVICES_ZONE=america
+TXT_FILE_NAME=na
+gcloud container clusters get-credentials --zone ${k8s_cluster_us_zone} ${k8s_cluster_us_name}
+mkdir -p ${DEVICES_ZONE}
+rm -rf ${DEVICES_ZONE}/*
+for aCity in $(cat ../06-device/data/${TXT_FILE_NAME}.txt); do
+  export CUSTER_REGION=${DEVICES_ZONE}
+  export CITY=${aCity}
+  export CITY_LOWER=$(echo ${aCity} | tr '[:upper:]' '[:lower:]')
+  cat device-tpl.yaml | envsubst > ${DEVICES_ZONE}/k8s-job-${CITY_LOWER}.yaml
+done
+kubectl apply -f ${DEVICES_ZONE}
+echo "-------------------------------------------------------------------------"
+
+
+echo "========================================================================="
+echo ""
+echo "  Launching ASIAN devices ..."
+echo ""
+echo "-------------------------------------------------------------------------"
+DEVICES_ZONE=asia
+TXT_FILE_NAME=oriente
+gcloud container clusters get-credentials --zone ${k8s_cluster_asia_zone} ${k8s_cluster_asia_name}
+mkdir -p ${DEVICES_ZONE}
+rm -rf ${DEVICES_ZONE}/*
+for aCity in $(cat ../06-device/data/${TXT_FILE_NAME}.txt); do
+  export CUSTER_REGION=${DEVICES_ZONE}
+  export CITY=${aCity}
+  export CITY_LOWER=$(echo ${aCity} | tr '[:upper:]' '[:lower:]')
+  cat device-tpl.yaml | envsubst > ${DEVICES_ZONE}/k8s-job-${CITY_LOWER}.yaml
+done
+kubectl apply -f ${DEVICES_ZONE}
+echo "-------------------------------------------------------------------------"
+
+
+echo "========================================================================="
+echo ""
+echo "  Launching ASIAN devices ..."
+echo ""
+echo "-------------------------------------------------------------------------"
+DEVICES_ZONE=oceania
+TXT_FILE_NAME=oceania
+mkdir -p ${DEVICES_ZONE}
+rm -rf ${DEVICES_ZONE}/*
+for aCity in $(cat ../06-device/data/${TXT_FILE_NAME}.txt); do
+  export CUSTER_REGION=${DEVICES_ZONE}
+  export CITY=${aCity}
+  export CITY_LOWER=$(echo ${aCity} | tr '[:upper:]' '[:lower:]')
+  cat device-tpl.yaml | envsubst > ${DEVICES_ZONE}/k8s-job-${CITY_LOWER}.yaml
+done
+kubectl apply -f ${DEVICES_ZONE}
+echo "-------------------------------------------------------------------------"
 
 #gcloud container clusters get-credentials --zone ${k8s_cluster_us_zone} ${k8s_cluster_us_name}
 #gcloud container clusters get-credentials --zone ${k8s_cluster_asia_zone} ${k8s_cluster_asia_name}
